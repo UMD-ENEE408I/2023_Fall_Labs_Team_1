74d0a5d511b9b0842ba5af07794ef43932ada3e0
Kent Chesley <kchesle1@umd.edu>
2023-09-08 18:52:49 -0400

    Adapted IMU code as required.

--- Tutorial_1/IMU/src/main.cpp
+++ Tutorial_1/IMU/src/main.cpp
@@ -3,11 +3,65 @@
 
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
+#include <chrono>  // For using now() function.
 
 Adafruit_MPU6050 mpu;
 
 const unsigned int ADC_1_CS = 2;
 const unsigned int ADC_2_CS = 17;
+
+const unsigned int M1_IN_1 = 13;
+const unsigned int M1_IN_2 = 12;
+const unsigned int M2_IN_1 = 25;
+const unsigned int M2_IN_2 = 14;
+
+const unsigned int M1_IN_1_CHANNEL = 8;
+const unsigned int M1_IN_2_CHANNEL = 9;
+const unsigned int M2_IN_1_CHANNEL = 10;
+const unsigned int M2_IN_2_CHANNEL = 11;
+
+const unsigned int M1_I_SENSE = 35;
+const unsigned int M2_I_SENSE = 34;
+
+const float M_I_COUNTS_TO_A = (3.3 / 1024.0) / 0.120;
+
+const unsigned int PWM_VALUE = 85; // Max PWM given 8 bit resolution. Set fairly low to prevent overshoot.
+
+const int freq = 5000;
+const int ledChannel = 0;
+const int resolution = 8;
+
+
+void M1_backward() {
+  ledcWrite(M1_IN_1_CHANNEL, PWM_VALUE);
+  ledcWrite(M1_IN_2_CHANNEL, 0);
+}
+
+void M1_forward() {
+  ledcWrite(M1_IN_1_CHANNEL, 0);
+  ledcWrite(M1_IN_2_CHANNEL, PWM_VALUE);
+}
+
+void M1_stop() {
+  ledcWrite(M1_IN_1_CHANNEL, 0);
+  ledcWrite(M1_IN_2_CHANNEL, 0);
+}
+
+void M2_backward() {
+  ledcWrite(M2_IN_1_CHANNEL, PWM_VALUE);
+  ledcWrite(M2_IN_2_CHANNEL, 0);
+}
+
+void M2_forward() {
+  ledcWrite(M2_IN_1_CHANNEL, 0);
+  ledcWrite(M2_IN_2_CHANNEL, PWM_VALUE);
+}
+
+void M2_stop() {
+  ledcWrite(M2_IN_1_CHANNEL, 0);
+  ledcWrite(M2_IN_2_CHANNEL, 0);
+}
+
 
 void setup(void) {
   // Stop the right motor by setting pin 14 low
@@ -99,15 +153,37 @@ void setup(void) {
 
   Serial.println("");
   delay(100);
+
+  ledcSetup(M1_IN_1_CHANNEL, freq, resolution);
+  ledcSetup(M1_IN_2_CHANNEL, freq, resolution);
+  ledcSetup(M2_IN_1_CHANNEL, freq, resolution);
+  ledcSetup(M2_IN_2_CHANNEL, freq, resolution);
+
+  ledcAttachPin(M1_IN_1, M1_IN_1_CHANNEL);
+  ledcAttachPin(M1_IN_2, M1_IN_2_CHANNEL);
+  ledcAttachPin(M2_IN_1, M2_IN_1_CHANNEL);
+  ledcAttachPin(M2_IN_2, M2_IN_2_CHANNEL);
+
+  pinMode(M1_I_SENSE, INPUT);
+  pinMode(M2_I_SENSE, INPUT);
+
 }
+
+// Initializations.
+double integral = 0;  // Used to integrate (Riemann sum) the angular velocity.
+double sample = 0;
+auto last_time = std::chrono::high_resolution_clock::now(); // For reference, see Sumsuddin Shojib' answer here: 
+// https://stackoverflow.com/questions/728068/how-to-calculate-a-time-difference-in-c
+double elapsed_time_ms = 0;
 
 void loop() {
 
   /* Get new sensor events with the readings */
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
-
+  
   /* Print out the values */
+  /*
   Serial.print("Acceleration X: ");
   Serial.print(a.acceleration.x);
   Serial.print(", Y: ");
@@ -127,4 +203,41 @@ void loop() {
   Serial.print("Temperature: ");
   Serial.print(temp.temperature);
   Serial.println(" degC");
+  */
+
+  
+  //Rotate 90 deg.
+  M1_forward();
+  M2_backward();
+  last_time = std::chrono::high_resolution_clock::now();  // Time in ms.
+  integral = 0;
+  do
+  {
+    // "Integrate" the angular velocity to get angular change.
+    delay(10);
+    mpu.getEvent(&a, &g, &temp);  // Update measurment.
+    sample = g.gyro.z;
+    elapsed_time_ms = std::chrono::duration<double, std::milli>(std::chrono::high_resolution_clock::now() - last_time).count();
+    // For reference, see Sumsuddin Shojib' answer here: 
+    // https://stackoverflow.com/questions/728068/how-to-calculate-a-time-difference-in-c
+    last_time = std::chrono::high_resolution_clock::now();
+    integral = integral + sample*elapsed_time_ms/1000;
+  } while (abs(integral) < 3.1416/2);
+  
+  //Rotate 180 deg in the opposited direction.
+  M1_backward();
+  M2_forward();
+  last_time = std::chrono::high_resolution_clock::now();  // Time in ms.
+  integral = 0;
+  do
+  {
+    // "Integrate" the angular velocity to get angular change.
+    delay(10);
+    mpu.getEvent(&a, &g, &temp);
+    sample = g.gyro.z;
+    elapsed_time_ms = std::chrono::duration<double, std::milli>(std::chrono::high_resolution_clock::now() - last_time).count();
+    last_time = std::chrono::high_resolution_clock::now();
+    integral = integral + sample*elapsed_time_ms/1000;
+  } while (abs(integral) < 3.1416);
+  
 }
