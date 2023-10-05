6cfea19d547d74a7fe6dcf95b950c85ae06af5ca
Kent Chesley <kchesle1@umd.edu>
2023-10-05 01:47:40 -0400

    Integrated Maze/Line_Following with Maze/State_Machine. Removed Line_Following folder since its functionality had been integrated with State_Machine.

--- Maze/Line_Following/.pio/build/heltec_wifi_kit_32_v2/FrameworkArduino/USBMSC.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/heltec_wifi_kit_32_v2/FrameworkArduino/USBMSC.cpp.o: \
- C:/Users/danie/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.cpp \
- C:/Users/danie/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
- C:/Users/danie/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
